82be2ab8af5ba24c62d853d736093919e3a6a42d
Ratul <gratul311@gmail.com>
2026-06-02 02:25:45 +0600

    add simulated_annealing.py - initial setup

---
+++ simulated_annealing.py
@@ -0,0 +1,9 @@
+# simulated_annealing.py
+# Author: Ratul Ghosh | ID: 0112410038
+# Branch: optimization_0112410038
+# Algorithm: Simulated Annealing
+
+algorithm_name = 'Simulated Annealing'
+student_id = '0112410038'
+initial_temperature = 100.0
+cooling_rate = 0.99
