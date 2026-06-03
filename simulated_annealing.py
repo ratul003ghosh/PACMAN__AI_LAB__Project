8836142883f23da21ba95cf21af1fddb270ba525
Ratul <gratul311@gmail.com>
2026-06-03 23:45:21 +0600

    Added SimulatedAnnealing class skeleton

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -7,3 +7,10 @@ algorithm_name = 'Simulated Annealing'
 student_id = '0112410038'
 initial_temperature = 100.0
 cooling_rate = 0.99
+
+class SimulatedAnnealing:      # class for the Simulated Annealing algorithm
+
+    # Constructor method
+    def __init__(self):
+        self.temperature = initial_temperature          # Initial temperature for exploration
+        self.cooling_rate = cooling_rate                   # Cooling rate to decrease the temperature after each iteration
