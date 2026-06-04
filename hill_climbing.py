2cc90fbfa395276b059559e02be9b1d073578501
Ratul <gratul311@gmail.com>
2026-06-04 19:37:07 +0600

    Added state and score attributes

--- hill_climbing.py
+++ hill_climbing.py
@@ -7,5 +7,12 @@ algorithm_name = 'Hill Climbing'
 student_id = '0112410038'
 
 class HillClimbing:
+
+    # Constructor method
     def __init__(self):
-        pass
+
+        # Store the current state being evaluated
+        self.current_state = None
+
+        # Store the score of the current state
+        self.current_score = None
