2110dd21db138690b6b8045294c3a268b1aa0ed0
Shihab0049 <shihabunshakib6@gmail.com>
2026-06-07 15:55:15 +0600

    Set Color.py

--- Enviroment.py
+++ Enviroment.py
@@ -14,3 +14,12 @@ HEIGHT = ROWS * CELL_SIZE
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("AI Pac-Man Maze Environment")
+
+# Colors
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+GRAY = (70, 70, 70)
