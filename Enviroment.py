8d83e7aabeee82fb0365ae1b39f84423d548d639
Shihab0049 <shihabunshakib6@gmail.com>
2026-06-08 23:18:24 +0600

    Maze_1.py

--- Enviroment.py
+++ Enviroment.py
@@ -23,3 +23,23 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 GRAY = (70, 70, 70)
+
+
+# Maze 1 (Easy)
+maze1 = [
+list("WWWWWWWWWWWWWWW"),
+list("WP   F      G W"),
+list("W WWW WWWWWWW W"),
+list("W             W"),
+list("W   F         W"),
+list("W WWWWWWWWWWW W"),
+list("W             W"),
+list("W G           W"),
+list("W      F      W"),
+list("W WWWWWWWWWWW W"),
+list("W             W"),
+list("W             W"),
+list("W         E   W"),
+list("W             W"),
+list("WWWWWWWWWWWWWWW")
+]
