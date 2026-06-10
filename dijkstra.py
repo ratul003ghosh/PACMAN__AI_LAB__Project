aec7157579c341b672ed5f7cbdd1e09138c8e660
IstiaqExe <illutionistgamer111@gmail.com>
2026-06-10 09:55:46 +0600

    dijkstra-v2

--- dijkstra.py
+++ dijkstra.py
@@ -31,3 +31,30 @@ def get_key_points(grid):
         "foods": find_all_symbols(grid, FOOD),
         "ghosts": find_all_symbols(grid, GHOST),
     }
+
+def is_walkable(grid, position, avoid_ghosts=True):
+    if not in_bounds(grid, position):
+        return False
+    row, col = position
+    cell = grid[row][col]
+    if cell == WALL:
+        return False
+    if avoid_ghosts and cell == GHOST:
+        return False
+    return True
+
+def get_neighbors(grid, position, avoid_ghosts=True):
+    row, col = position
+
+    possible_moves = [
+        (row - 1, col),  #Up
+        (row + 1, col),  #Down
+        (row, col - 1),  #Left
+        (row, col + 1),  #Right
+    ]
+
+    neighbors = []
+    for next_position in possible_moves:
+        if is_walkable(grid, next_position, avoid_ghosts=avoid_ghosts):
+            neighbors.append(next_position)
+    return neighbors
